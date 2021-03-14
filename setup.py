5a5d406165f2628b85ba3c3abb7bbe6a4550ae4a
KamalAyoubi <kamal.ayoubi@etu.umontpellier.fr>
2021-03-14 16:26:37 +0100

    change project name

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 #from biketrauma import __version__ as current_version
 
 setup(
-  name='bike_trauma',
+  name='bike_3trauma',
   version= '0.0.1',
   description='Visualization of a bicycle accident db',
   url='https://github.com/KamalAyoubi/packaging_tutorial',
